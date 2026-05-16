c6d7eb90c50309bf56239494243e2162636ab9d2
zxc12567-coder <zxc12567@gmail.com>
2026-05-16 23:56:23 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -193,8 +193,8 @@ export default function App() {
   useEffect(()=>{
     (async()=>{
       let data=[];
-      if(SUPABASE_URL){ setSyncing(true); const {data:d,error}=await sb.select(TABLE); if(!error){data=d?.length?d:data;setIsOnline(true);}{data=d;setIsOnline(true);}else data=await idbAll(); setSyncing(false); }
-      else data=await idbAll();
+      if(SUPABASE_URL){ setSyncing(true); const {data:d,error}=await sb.select(TABLE); if(!error){data=d?.length?d:data;setIsOnline(true);}else data=await idbAll(); setSyncing(false); }
+    
       setItems(data); setLoaded(true);
     })();
     idbGet("botLog").then(v=>v&&setBotLog(v));
